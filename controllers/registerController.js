e47316ab7d825ea2b7af3e65f2d4d2335714c1eb
twong17 <70173433+twong17@users.noreply.github.com>
2021-05-19 11:57:41 -0400

    Update registerController.js

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,30 +1,29 @@
 const { body,validationResult } = require("express-validator");
 var UserInfo = require('../models/userinfo');
 
-//exports.index = function(req, res) {
-//     res.render('index', { title: 'Trove Login Page'});
-//};
 
-// Display UserInfo form on GET.
+// Render register page
 exports.register = function(req, res) {
-    res.render('register', { title: 'Trove Register Page'});
+    res.render('register', { title: 'PitchingGuru Register Page'});
 };
 
-// Display UserInfo create form on GET.
+// Render register page
 exports.register_create_get = function(req, res) {
-    res.render('register', { title: 'Trove Register Page'});
+    res.render('register', { title: 'PitchingGuru Register Page'});
 };
 
-// Handle userinfo create on POST.
+// Create new user 
 exports.register_create_post =  [
    
 //  // Validate and santise the name field.
   body('username', 'Username required').trim().isLength({ min: 1 }).escape(),
   body('password', 'Password required').trim().isLength({ min: 1 }).escape(),
   body('name', 'Name required').trim().isLength({ min: 1 }).escape(),
-//  body('lastname', 'Last name required').trim().isLength({ min: 1 }).escape(),
-//  body('email', 'Email required').trim().isLength({ min: 1 }).escape(),
-//  body('location', 'Location required').trim().isLength({ min: 1 }).escape(),
+    body('team', 'Team required').trim().isLength({ min: 1 }).escape(),
+    body('position', 'Position required').trim().isLength({ min: 1 }).escape(),
+    body('handedness', 'Handedness required').trim().isLength({ min: 1 }).escape(),
+    body('height', 'Height required').trim().isLength({ min: 1 }).escape(),
+    body('weight', 'Weight required').trim().isLength({ min: 1 }).escape(),
 
   // Process request after validation and sanitization.
   (req, res, next) => {
@@ -48,6 +47,11 @@ exports.register_create_post =  [
         password: req.body.password,
 //        email: req.body.email,
         name: req.body.name,
+        team: req.body.team,
+        position: req.body.position,
+        handedness: req.body.handedness,
+        height: req.body.height,
+        weight: req.body.weight,
 //        location: req.body.location,
 //        lastname: req.body.lastname
       }
@@ -59,7 +63,8 @@ exports.register_create_post =  [
                
                 req.session.newuser = userinfo;
                 //res.redirect('/catalog/register/image');
-                res.send('Success');
+                //res.send('Success');
+                res.redirect('/catalog/register/image');
              });  
         }
   }
